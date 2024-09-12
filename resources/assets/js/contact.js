ee5dd1c22309b20dba21b0e14e352a29af51f936
Brian <briangomezmartinezz@gmail.com>
2024-09-12 21:38:32 +0200

    New portfolio

---
+++ resources/assets/js/contact.js
@@ -0,0 +1,28 @@
+// Inicializa EmailJS
+(function() {
+    emailjs.init("XQzJdLZ3yaqhrTjOJ"); // Reemplaza TU_USER_ID con tu user ID de EmailJS
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Obtiene los valores del formulario
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value; // Correo del destinatario
+    let message = document.getElementById("message").value;
+
+    // Parámetros del servicio de EmailJS
+    let params = {
+        name: name,
+        email: email, // Asegúrate de que este campo coincida con el nombre del parámetro en tu plantilla
+        message: message
+    };
+
+    // Envía el correo utilizando EmailJS
+    emailjs.send("service_ou4dz0t", "template_htwh7fb", params)
+    .then(function(response) {
+        alert("Correo enviado correctamente.");
+    }, function(error) {
+        alert("Ocurrió un error al enviar el correo.");
+    });
+});
