6a3776848903ece11261cf197e727091b9d72c32
Brian Gomez Martinez <briangomezmartinezz@gmail.com>
2024-11-15 14:35:50 +0100

    Catchap added

--- resources/assets/js/contact.js
+++ resources/assets/js/contact.js
@@ -1,28 +1,57 @@
-// Inicializa EmailJS
-(function() {
-    emailjs.init("XQzJdLZ3yaqhrTjOJ"); // Reemplaza TU_USER_ID con tu user ID de EmailJS
-})();
+    (function() {
+        emailjs.init("XQzJdLZ3yaqhrTjOJ"); // Reemplaza con tu user ID de EmailJS
+    })();
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+    const submissionCooldown = 60 * 60 * 1000; // 1 hora en milisegundos
+    const maxSubmissions = 5; // Número máximo de envíos permitidos por hora
 
-    // Obtiene los valores del formulario
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+    function canSubmit() {
+        const currentTime = new Date().getTime();
+        const submissionData = JSON.parse(localStorage.getItem('submissionData')) || { count: 0, firstSubmissionTime: currentTime };
 
-    // Parámetros del servicio de EmailJS
-    let params = {
-        name: name,
-        email: email,
-        message: message
-    };
+        if (currentTime - submissionData.firstSubmissionTime >= submissionCooldown) {
+            submissionData.count = 0;
+            submissionData.firstSubmissionTime = currentTime;
+        }
 
-    // Envía el correo utilizando EmailJS
-    emailjs.send("service_ou4dz0t", "template_htwh7fb", params)
-    .then(function(response) {
-        alert("Correo enviado correctamente.");
-    }, function(error) {
-        alert("Ocurrió un error al enviar el correo.");
+        if (submissionData.count >= maxSubmissions) {
+            alert("Has alcanzado el límite de envíos por hora. Por favor, espera antes de intentar nuevamente.");
+            return false;
+        } else {
+            submissionData.count += 1;
+            localStorage.setItem('submissionData', JSON.stringify(submissionData));
+            return true;
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (!canSubmit()) {
+            return;
+        }
+
+        // Obtiene los valores del formulario
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let message = document.getElementById("message").value;
+
+        // Parámetros del servicio de EmailJS
+        let params = {
+            name: name,
+            email: email,
+            message: message
+        };
+
+        // Envía el correo utilizando EmailJS
+        emailjs.send("service_ou4dz0t", "template_htwh7fb", params)
+            .then(function(response) {
+                alert("Correo enviado correctamente.");
+            }, function(error) {
+                alert("Ocurrió un error al enviar el correo.");
+            });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.getElementById('contactForm');
+        form.addEventListener('submit', handleSubmit);
     });
-});
